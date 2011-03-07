6f1747c92382fae1183fa7f9922aed3bb3d41625
ihaywood3 <ihaywood3@04e54bd1-ce00-af76-269f-b9dc7ec687a5>
2011-03-07 08:39:05 +0000

    further GUI changes

--- trunk/lib/gui.rb
+++ trunk/lib/gui.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'fox16'
 require 'interfaces'
+require 'thread'
+
 include Fox
 
 module Athen
@@ -18,8 +20,8 @@ module Athen
       lbl1.font = font
       @status.font = font
       @status.textColor = FXRGB(10,220,10)
-      @text = FXText.new(packer,
-                         :opts=>LAYOUT_FILL)
+      @text = FXText.new(packer,:opts=>LAYOUT_FILL)
+      @log = []
       hframe2 = FXHorizontalFrame.new(packer,
                                      :opts => LAYOUT_SIDE_BOTTOM|LAYOUT_FILL_X)
       download_now = FXButton.new(hframe2, "Download Now",:opts => BUTTON_NORMAL|BUTTON_AUTOGRAY|LAYOUT_LEFT)
@@ -34,17 +36,30 @@ module Athen
       quit.connect(SEL_COMMAND) { app.stop }
       quit.tipText = "Exits the program"
       #download_now.connect(SEL_UPDATE) { true }
+      # setup timer to handle messages from other threads
+      app.addTimeout(500, method(:timer), :repeat => true)
     end
  	
-
-    def bad_status(s)
-      @status.textColor = FXRGB(10,220,10)
-      @status.text = s
+    def timer(sender,selector,data)
+      while not @iface.queue.empty?
+        h = @iface.queue.pop
+        log(h[:log]) if h.key? :log
+        if h.key? :status
+          @status.text = h[:log] if h.key? :log
+          @status.textColor = case h[:status]
+                when :ok then FXRGB(10,220,10) # green
+                when :warning then FXRGB(220,220,10) # yellow
+                when :bad then FXRGB(220,10,10) # red
+          end
+        end
+      end
     end
 
-    def ok_status()
-      @status.textColor = FXRGB(10,220,10)
-      @status.text = "OK"
+    def log(s)
+      lines = h[:log].split("\n")
+      @log += lines
+      @log = @log[-50..-1] if @log.length > 51
+      @text.text = @log.join("\n")
     end
 
     def create
@@ -78,13 +93,18 @@ module Athen
       @matrix = FXMatrix.new(self, 3, :opts => MATRIX_BY_COLUMNS|LAYOUT_FILL|LAYOUT_SIDE_TOP)
       @font = FXFont.new(FXApp.instance, "System", 8, :hints=>FXFont::System)
       smtp_host = text_field_row("SMTP Host", "The server provided by your ISP or e-mail\nprovider to send e-mails out")
+      smtp_host.text = $cfg['smtp'] or ''
       smtp_user = text_field_row("SMTP Username","Username to log on to\nthe SMTP host, optional")
+      smtp_user.text = $cfg['smtp_user'] or ''
       smtp_password = text_field_row("SMTP Password","Password to log on to\nthe SMTP host, optional")
+      
       error_email = text_field_row("Error E-mail","E-mail to send error reports to")
-      FXLabel.new(@matrix, "Protocol", :opts=>JUSTIFY_LEFT)
-      field = FXTextField.new(@matrix, 50, :opts=>style)
-      lbl1 = FXLabel.new(@matrix, "Protocol ", :opts=>JUSTIFY_LEFT|LAYOUT_FILL_X|LAYOUT_FILL_COLUMN)
-      lbl1.font = @font
+      imap_host = text_field_row("IMAP Host", "The server provided by your ISP or e-mail\nprovider to receive e-mails")
+      imap_user = text_field_row("IMAP Username","Username to log on to\nthe IMAP host, optional")
+      imap_password = text_field_row("IMAP Password","Password to log on to\nthe IMAP host, optional")
+      upload_dir = dir_field_row("Upload directory","The directory where ATHEN finds files for upload")
+      download_dir = dir_field_row("Download directory","The directory where ATHEN saves incoming files")
+      ack_dir = dir_field_row("ACK directory","The directory where ACK files are found\nLeave blank if not used")
       # Accept
       accept = FXButton.new(buttons, "&Accept", :opts=>FRAME_RAISED|FRAME_THICK|LAYOUT_RIGHT|LAYOUT_CENTER_Y)
       accept.connect(SEL_COMMAND, method(:save))
@@ -103,8 +123,22 @@ module Athen
         style = style|TEXTFIELD_PASSWD
       end
       field = FXTextField.new(@matrix, 50, :opts=>style)
-      lbl1 = FXLabel.new(@matrix, comment, :opts=>JUSTIFY_LEFT|LAYOUT_FILL_X|LAYOUT_FILL_COLUMN)
+      lbl1 = FXLabel.new(@matrix, comment, :opts=>JUSTIFY_LEFT|LAYOUT_FILL_X|LAYOUT_SIDE_TOP|LAYOUT_FILL_COLUMN)
       lbl1.font = @font
+      return field
+    end
+
+    def dir_field_row(lbl, comment)
+      FXLabel.new(@matrix, lbl, :opts=>JUSTIFY_LEFT)
+      hbox = FXHorizontalFrame.new(@matrix,:opts=>LAYOUT_FILL_X|LAYOUT_FILL_COLUMN)
+      field = FXTextField.new(hbox, 50, :opts=> LAYOUT_FILL_X|TEXTFIELD_NORMAL)
+      btn = FXButton.new(hbox,"Find...",:opts=>FRAME_RAISED|FRAME_THICK)
+      lbl1 = FXLabel.new(@matrix, comment, :opts=>JUSTIFY_LEFT|LAYOUT_FILL_X|LAYOUT_SIDE_TOP|LAYOUT_FILL_COLUMN)
+      lbl1.font = @font
+      btn.connect(SEL_COMMAND) do
+        d = FXDirDialog.getOpenDirectory(self,lbl,field.text or "")
+        field.text = d if d
+      end
       return field
     end
 
@@ -119,6 +153,7 @@ module Athen
       @log = []
       @app = a
       @main_window = mw
+      @queue = Queue.new
     end
 
    def GuiInterface.run
@@ -129,23 +164,17 @@ module Athen
         app.run
       end
    end
+   
+   def queue
+     @queue
+   end
+   
+   def log(s)
+     @queue.push {:log=>s}
+   end
 
-    def log(s)
-      lines = s.split("\n")
-      @log += lines
-      @log = @log[-50..-1] if @log.length > 51
-      @main_window.display_log(@log)
-    end
-
-    def panic(s)
-      FXMessageBox.error(@app,MBOX_OK,"Error",s)
-      l = s.split("\n")
-      @main_window.badstatus(l.first)
-    end
-
-    def ok_status()
-      @main_window.ok_status()
-    end
-
+   def status(s,m)
+     @queue.push {:log=>m,:status=>s}
+   end
   end
 end
