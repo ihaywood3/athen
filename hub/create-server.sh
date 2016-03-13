db94ed763d7378ec8c981469fcedf67ad9df8745
Ian Haywood <ian@haywood.id.au>
2016-03-14 10:14:03 +1100

    clean up script

--- hub/create-server.sh
+++ hub/create-server.sh
@@ -1,5 +1,4 @@
-
-
+#!/bin/sh
 # install requirements
 # slapd will prompt for password, slapd username is "cn=admin,dc=nodomain"
 apt-get install -y --force-yes --no-install-recommends slapd ldap-utils apache2 python supervisor texlive-latex-base cups-client python-m2crypto python-ldap python-flask
@@ -9,6 +8,4 @@ ldapadd -x -D "cn=admin,dc=athen,dc=net,dc=au" -W -f base.ldif # will ask for pa
 cp athen.pem /etc/ssl/certs/
 cp athen.key /etc/ssl/private/
 chmod 400 /etc/ssl/private/athen.key
-EXPOSE 80 389 443
-CMD ["/usr/bin/supervisord"]
-#ENTRYPOINT /bin/bash
+
