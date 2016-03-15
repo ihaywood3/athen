c9cae7b9eb4225b23e94d96e52a167a86ca961f5
Ian Haywood <ian@haywood.id.au>
2016-03-15 12:09:54 +1100

    make script a bit nicer

--- hub/create-server.sh
+++ hub/create-server.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 # install requirements
 # slapd will prompt for password, slapd username is "cn=admin,dc=nodomain"
-apt-get install -y --force-yes --no-install-recommends slapd ldap-utils apache2 python supervisor texlive-latex-base cups-client python-m2crypto python-ldap python-flask
+set +x
+set +e
+apt-get install -y --no-install-recommends slapd ldap-utils apache2 python texlive-latex-base cups-client python-m2crypto python-ldap python-flask
 ldapadd -Y EXTERNAL -H ldapi:/// -f athen.ldif
 ldapmodify -Y EXTERNAL -H ldapi:/// -f config.ldif 
 ldapadd -x -D "cn=admin,dc=athen,dc=net,dc=au" -W -f base.ldif # will ask for password again here
-cp athen.pem /etc/ssl/certs/
-cp athen.key /etc/ssl/private/
-chmod 400 /etc/ssl/private/athen.key
+#cp athen.pem /etc/ssl/certs/
+#cp athen.key /etc/ssl/private/
+#chmod 400 /etc/ssl/private/athen.key
 
