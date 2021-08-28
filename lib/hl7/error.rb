41b4a3fe910a6462ecb19ef2551e66ee5d982e2d
Ian Haywood <ian@haywood.id.au>
2021-08-28 18:41:41 +1000

    move lib to athen/

--- lib/hl7/error.rb
+++
@@ -1,5 +0,0 @@
-# an HL7 parsing error
-module HL7
-  class Error < StandardError
-  end
-end
--- lib/hl7/hl7_pp.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-$: << "/home/ian/athen/lib"
-require 'hl7/message'
-pp HL7::Message.parse(STDIN.read)
